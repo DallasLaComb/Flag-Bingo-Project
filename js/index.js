ae0b50b2999facc89ced385fec74ad9c63769f00
Dallas LaComb <dallaslacombdrive@gmail.com>
2023-10-28 20:04:13 -0400

    Added new html pages. The functionality is going to change on this from previous ones... I thought we shouldn't have new html pages at first, but after seeing how cookies work, I think it's better to have seperate pages for simplicity of maintanence...

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ function loadData(callback) {
 function processData() {
   // processData function is where the countryData is processed and displayed on the page.
   // Create continent arrays in continents object.
+
   const continents = {
     na: [],
     eu: [],
@@ -43,17 +44,15 @@ function processData() {
   }
   //   console.log(countryData);
   let selectedFlags = [];
+  // Array that will hold the selected flags. This will be loaded into savedCountryCodes when the save button is clicked...
   for (let i = 0; i < countryData.length; i++) {
     let countryCode = countryData[i].countryCode.toLowerCase(); // country code is used for the image source. EX: us.png == ${countryCode}.png
     let continent = countryData[i].continent; // continent is used to determine which div the country will be appended to: EX: NA == .na, EU == .eu, etc.
     let countryName = countryData[i].countryName; // countryName is used to display the country name on the page: EX: United States
     let countryImage = `<img class="img-fluid" src="imagesSmall/${countryCode}.png" alt=${countryName}>`; // countryImage is used to display the country flag on the page: EX: <img src="imagesSmall/us.png"> displays US Flag
-    // newCountryCard is the div that is created for each country. It is appended to the appropriate continent div.
-    // EX: <div class='col-2-sm pt-3 border-top mt-3' id='us'>United States<br><img class="img-fluid" src="imagesSmall/us.png"></div>
-    // newCountryCards also get added new on click functionality.
 
     let newCountryCard = $(
-      `<div class='col-2-sm pt-3 border-top mt-3' id=${countryCode}>${countryName}<br class ="hidden">${countryImage}<i class="bi bi-app "></i></div>`
+      `<div class='col-2-sm pt-3 border-top mt-3' id=${countryCode}>${countryName}<br class ="hidden">${countryImage}<i class="bi bi-app"></i></div>`
     ).on("click", function () {
       if ($(this).hasClass("selected")) {
         let index = selectedFlags.indexOf(this);
@@ -61,15 +60,18 @@ function processData() {
         $(this).removeClass("selected");
 
         $(this).children().removeClass("bi-check-square");
-        console.log(selectedFlags);
+        // console.log(selectedFlags);
       } else {
         $(this).children().addClass("bi-check-square");
         $(this).addClass("selected");
         console.log(this);
         selectedFlags.push(this);
-        console.log(selectedFlags);
+        // console.log(selectedFlags);
       }
     });
+    // ^^ newCountryCard is the div that is created for each country. It is appended to the appropriate continent div.
+    // EX: <div class='col-2-sm pt-3 border-top mt-3' id='us'>United States<br><img class="img-fluid" src="imagesSmall/us.png"></div>
+    // newCountryCards also get added new on click functionality.
 
     // Appends the newCountryCard div to the appropriate continent div. Appends means to add to the end of the current html div.
     switch (continent) {
@@ -93,6 +95,7 @@ function processData() {
         break;
     }
   }
+  // ^^ Ending brackect of for loop that creates newCountryCards for each country from the data in the json file.
   $("#save-button").on("click", function () {
     $("i").remove();
     $("#callGenerator").removeClass("deactivate");
@@ -100,29 +103,27 @@ function processData() {
     $("#flagSelectionContainer").addClass("deactivate");
     $(".availableList").append(selectedFlags);
     console.log(selectedFlags);
-
-    //Saves the country codes for each flag selected after the save button is clicked
+    // ^^ save-button id is the button in the bottom right that says "Saved Cards".
+    // ^^ When "Saved Cards" is clicked, the selectedFlags array is appended to the .availableList div.
+    // ^^ The logic on this is going to change a lot, but for now, it removes the save button and the flags from the page, and adds the selectedFlags array to the .availableList div.
     let savedCountryCodes = [];
+    // Array that will hold the country codes of the selected flags.
     for (let i = 0; i < selectedFlags.length; i++) {
       savedCountryCodes[i] = $(selectedFlags[i]).attr("id");
-      console.log("Saved Country Codes: " + savedCountryCodes[i]);
+      // ^^ This for loop traverses through the selectedFlags array and adds the country codes (which is given to them as an ID) to the savedCountryCodes array.
+      // console.log("Saved Country Codes: " + savedCountryCodes[i]);
     }
-
-    //Flag names get saved to cookies
     let flagsToCookies = JSON.stringify(savedCountryCodes);
+    // ^^ This converts the savedCountryCodes array into a string so that it can be stored in a cookie.
     Cookies.set("myCookie", flagsToCookies, { path: "/" });
+    // ^^ This sets the cookie. The first parameter is the name of the cookie, the second parameter is the value of the cookie, and the third parameter is the path of the cookie.
   });
 }
+// ^^ Ending bracket of processData function.
 loadData(processData);
-// // Creating saved cards tab. When you click on the saved cards tab, it will display the saved cards via cookies...
-$("#savedCards").on("click", function () {
-  $("body > :not(nav)").remove();
-  // ADD ANYTHING FOR SAVED CARDS TAB BELOW THIS LINE
-  let jsonString = Cookies.get("myCookie");
-  let retrievedCookies = JSON.parse(jsonString);
-  console.log("Cookie Dump: " + retrievedCookies);
-  $("body").append(retrievedCookies);
-});
-// // let obj = [
-// //     {"lobbyname":"aw,bs,cr,ca,cr"},
-// // ]
+// ^^ The end of the hompage code. Code below this is for the other tabs...
+
+
+
+
+{/* <input type="text" id="my-input"> */}
