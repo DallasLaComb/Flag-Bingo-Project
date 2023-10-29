06cfd90693f7d0830dc1cf1cb467b9c7b8cdf85d
Dallas LaComb <dallaslacombdrive@gmail.com>
2023-10-28 23:48:23 -0400

    savedcards.html page update, can get cookies and paste the info...

--- js/savedCards.js
+++ js/savedCards.js
@@ -1,8 +1,12 @@
 // // Creating saved cards tab. When you click on the saved cards tab, it will display the saved cards via cookies...
     // ADD ANYTHING FOR SAVED CARDS TAB BELOW THIS LINE
-    let jsonString = Cookies.get("myCookie");
+    let jsonString = Cookies.get("lobbyData");
     let retrievedCookies = JSON.parse(jsonString);
-    console.log("Cookie Dump: " + retrievedCookies);
-    $("body").append(retrievedCookies);
+    for (let i=0; i<retrievedCookies.length; i++) {
+        let lobbyName = retrievedCookies[i].lobbyName;
+        $("body").append(lobbyName);
+    }
+    console.log("Cookie Dump: " + jsonString);
+  
 
   
